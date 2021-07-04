fadc05bf256c09c5308d16b5746fe1ae3c7699b4
Will Morris <teamexangus@gmail.com>
2021-07-04 15:14:47 +1000

    Version

--- RandomHealth/RandomHealth.cs
+++ RandomHealth/RandomHealth.cs
@@ -12,7 +12,7 @@ namespace RandomHealth
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
 
         private Handlers.Spawning Spawning;
 
